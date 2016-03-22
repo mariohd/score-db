1402da1355fb774742a3df7822aadadec84b4338
mariohd <mariohenriquediniz@gmail.com>
2016-03-22 17:58:35 -0300

    adding express

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+/*
 var http = require('http');
 
 http.createServer(function (request, response) {
 	response.writeHead(200, {"Content-type" : "text/plain" } );
 	response.end("Hello World\n");
 }).listen(process.env.PORT);
+*/ 
+var express = require('express');
+var app = express();
+
+app.get('/fetch', function (req, res) {
+	response.writeHead(200, {"Content-type" : "text/plain" } );
+	response.end("fetch success!");
+});
+
+app.get('/', function (req, res) {
+	response.writeHead(200, {"Content-type" : "text/plain" } );
+	response.end("/ success!");
+});
+
+var server = app.listen(process.env.PORT);
